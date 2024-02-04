ad83ae02356dba6cd12070dfd1f83d7e296934b4
Joseph Boateng <galileoboateng@gmail.com>
2024-02-04 04:01:57 +0000

    Addition of students.py and student_db.py

---
+++ student_db.py
@@ -0,0 +1,40 @@
+#lets create a student class and give certain attributes
+class Student:
+    valedictorian = None #This  is the class variable to store the valedictorian
+
+    def __init__(self, name, student_id, major, gpa, is_on_probation):
+        self.name = name
+        self.student_id = student_id
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation 
+        self.valedictorian = False #By default, students are not valedictorians
+#Now lets consider what will make a good student.
+# For a student to be deemed as a good student their gpa should be more than 3.0
+#If not then you are not part of the good student list
+    def good_student(self):
+        if self.gpa > 3.0:
+            return f"You will be part oof the good student list"
+        
+        else:
+            return f"You will not be part of the good student list"
+
+#now lets find sudent who can be eligible to be a software engineer
+#They mus satisfy two conditions, gpa > 3.0 and must be in the fields of mathematics computer science and software engineering     
+    def software_engineer(self):
+        if self.gpa >= 3.0 and self.major in ["Mathematics", "Computer science", "Statistics", "Computer engineering"]:
+            return f"You can be a software engineer"
+        else:
+            return f"You cannot be software engineer"
+        
+
+    def graduation(self):
+        if self.gpa < 1.5:
+            return f"You are on probation, you might not graduate"
+        else:
+            return f"you are good to graduate"
+        
+    #Now lets create a valedictorian for this school system
+    #Check if the student has the highest GPA and update valedictorian
+        
+        
---
+++ students.py
@@ -0,0 +1,42 @@
+from student_db import Student
+
+
+student1 = Student("Joseph", 109731, "Biochemistry", 3.50, True)
+student2 = Student("Benji", 123456, "Computer engineering", 2.74, True)
+student3 = Student("Eben", 246822, "Statistics", 3.40, False)
+student4 = Student("Berry", 987654, "Accounting", 3.0, True)
+student5 = Student("Edem", 135791, "Accounting", 2.34, False)
+student6 = Student("George", 222222, "Mathematics", 3.89, False)
+student7 = Student("Mussa", 000000, "Mathematics", 1.2, False)
+
+print(student1.good_student())
+print(student2.good_student())
+print(student3.good_student())
+print(student4.good_student())
+print(student5.good_student())
+print(student6.good_student())
+print(student7.good_student())
+
+print(student1.software_engineer())
+print(student2.software_engineer())
+print(student3.software_engineer())
+print(student4.software_engineer())
+print(student5.software_engineer())
+print(student6.software_engineer())
+print(student7.software_engineer())
+
+print(student1.graduation())
+print(student2.graduation())
+print(student3.graduation())
+print(student4.graduation())
+print(student5.graduation())
+print(student6.graduation())
+print(student7.graduation())
+
+print(student1.valedictorian())
+print(student2.valedictorian())
+print(student3.valedictorian())
+print(student4.valedictorian())
+print(student5.valedictorian())
+print(student6.valedictorian())
+print(student7.valedictorian())
